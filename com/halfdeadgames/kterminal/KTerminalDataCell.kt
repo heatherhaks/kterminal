6849241f1a54804f79ca263ac12ef3c0d5aed330
heatherhaks <heatherannekiefer@gmail.com>
2018-04-24 05:19:57 -0600

    Update KTerminalDataCell.kt

--- com/halfdeadgames/kterminal/KTerminalDataCell.kt
+++ com/halfdeadgames/kterminal/KTerminalDataCell.kt
@@ -5,4 +5,5 @@ import com.badlogic.gdx.graphics.Color
 data class KTerminalDataCell(var data: Char = ' ',
                              var foregroundColor: Color = Color.WHITE,
                              var backgroundColor: Color = Color.BLACK) {
+    var needsUpdate: Boolean = true
 }

7b018b302d5c4e88fe290dd9c344149038a43524
heatherhaks <heatherannekiefer@gmail.com>
2018-05-03 06:31:46 -0600

    added fix to support clipping via glScissor

--- src/com/halfdeadgames/offscreenrenderer/OffScreenRenderer.kt
+++ src/com/halfdeadgames/offscreenrenderer/OffScreenRenderer.kt
@@ -1,17 +1,20 @@
 package com.halfdeadgames.offscreenrenderer
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.graphics.glutils.FrameBuffer
+import com.badlogic.gdx.graphics.glutils.HdpiUtils.glViewport
 import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.utils.Disposable
 import ktx.app.clearScreen
 import ktx.app.use
 
-class OffScreenRenderer(width: Int, height: Int, private val batch: Batch, var clearScreen: Boolean = false) : Disposable {
+class OffScreenRenderer(var width: Int, var height: Int, private val batch: Batch, var clearScreen: Boolean = false) : Disposable {
     private val buffer = FrameBuffer(Pixmap.Format.RGBA8888, width, height, false)
 
     val textureRegion: TextureRegion
@@ -21,11 +24,11 @@ class OffScreenRenderer(width: Int, height: Int, private val batch: Batch, var c
         textureRegion.flip(false, true)
     }
 
-    fun render(batchColor: Color = Color.WHITE, clearColor: Color = Color.CLEAR,  block: (Batch) -> Unit): Texture {
+    fun render(batchColor: Color = Color.WHITE, clearColor: Color = Color.CLEAR, scissor: Boolean = false, block: (Batch) -> Unit): Texture {
         buffer.use {
             // clearing the color here to due a bug where fading a modal in or out changes the color of the batch and can affect the background opacity
             if(clearScreen) clearScreen()
-
+            if(scissor) glViewport(0, 0, width, height)
             batch.use(batchColor) {
                 block(batch)
             }

2114579bd225588a4b90e1ace685344a375a678e
heatherhaks <heatherannekiefer@gmail.com>
2018-05-13 23:35:08 -0600

    Delete KTerminalDataCell.kt

--- src/com/halfdeadgames/kterminal/KTerminalDataCell.kt
+++
@@ -1,9 +0,0 @@
-package com.halfdeadgames.kterminal
-
-import com.badlogic.gdx.graphics.Color
-
-data class KTerminalDataCell(var data: Char = ' ',
-                             var foregroundColor: Color = Color.WHITE,
-                             var backgroundColor: Color = Color.BLACK) {
-    var needsUpdate: Boolean = true
-}

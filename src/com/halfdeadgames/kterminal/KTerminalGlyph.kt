b74b8e21a93dfcca4e1968812b08ab22ef5a4500
heatherhaks <heatherannekiefer@gmail.com>
2018-05-17 05:20:19 -0600

    implemented a set method

--- src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
+++ src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
@@ -4,4 +4,10 @@ import com.badlogic.gdx.graphics.Color
 
 data class KTerminalGlyph(var char: Char,
                           var foregroundColor: Color,
-                          var backgroundColor: Color)
+                          var backgroundColor: Color) {
+    fun set(char: Char, foregroundColor: Color, backgroundColor: Color) {
+        this.char = char
+        this.foregroundColor = foregroundColor
+        this.backgroundColor = backgroundColor
+    }
+}

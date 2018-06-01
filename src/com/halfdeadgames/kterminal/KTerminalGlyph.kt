b91d669f0fc15e95e810f25e684e65bae4cea754
heatherhaks <heatherannekiefer@gmail.com>
2018-06-01 00:30:40 -0600

    improved java interop and fixed flip support

--- src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
+++ src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
@@ -2,24 +2,26 @@ package com.halfdeadgames.kterminal
 
 import com.badlogic.gdx.graphics.Color
 
-data class KTerminalGlyph(var char: Char,
+data class KTerminalGlyph @JvmOverloads constructor(var char: Char,
                           var foreground: Color,
                           var background: Color,
                           var rotation: Float = 0f,
-                          var isFlipped: Boolean = false) {
-    fun set(char: Char, foregroundColor: Color, backgroundColor: Color, rotation: Float = 0f, isFlipped: Boolean = false) {
+                          var isFlippedX: Boolean = false,
+                          var isFlippedY: Boolean = true) {
+    @JvmOverloads fun set(char: Char, foregroundColor: Color, backgroundColor: Color, rotation: Float = 0f, isFlippedX: Boolean = false, isFlippedY: Boolean = false) {
         this.char = char
         this.foreground = foregroundColor.cpy()
         this.background = backgroundColor.cpy()
         this.rotation = rotation
-        this.isFlipped = isFlipped
+        this.isFlippedX = isFlippedX
+        this.isFlippedY = isFlippedY
     }
 
     fun set(glyph: KTerminalGlyph) {
-        set(glyph.char, glyph.foreground, glyph.background, glyph.rotation, glyph.isFlipped)
+        set(glyph.char, glyph.foreground, glyph.background, glyph.rotation, glyph.isFlippedX, glyph.isFlippedY)
     }
 
     fun copy() : KTerminalGlyph {
-        return KTerminalGlyph(char,  foreground.cpy(), background.cpy(), rotation, isFlipped)
+        return KTerminalGlyph(char,  foreground.cpy(), background.cpy(), rotation, isFlippedX, isFlippedY)
     }
 }

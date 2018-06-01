8d4206836951ec6418a03dee403063591666c908
heatherhaks <heatherannekiefer@gmail.com>
2018-05-31 23:04:56 -0600

    implemented rotation for glyphs

--- src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
+++ src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
@@ -4,18 +4,20 @@ import com.badlogic.gdx.graphics.Color
 
 data class KTerminalGlyph(var char: Char,
                           var foreground: Color,
-                          var background: Color) {
-    fun set(char: Char, foregroundColor: Color, backgroundColor: Color) {
+                          var background: Color,
+                          var rotation: Float = 0f) {
+    fun set(char: Char, foregroundColor: Color, backgroundColor: Color, rotation: Float = 0f) {
         this.char = char
         this.foreground = foregroundColor.cpy()
         this.background = backgroundColor.cpy()
+        this.rotation = rotation
     }
 
     fun set(glyph: KTerminalGlyph) {
-        set(glyph.char, glyph.foreground, glyph.background)
+        set(glyph.char, glyph.foreground, glyph.background, glyph.rotation)
     }
 
     fun copy() : KTerminalGlyph {
-        return KTerminalGlyph(char,  foreground.cpy(), background.cpy())
+        return KTerminalGlyph(char,  foreground.cpy(), background.cpy(), rotation)
     }
 }

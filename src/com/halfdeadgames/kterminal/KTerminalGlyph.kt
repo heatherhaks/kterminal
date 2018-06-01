6d05968bce01fec41cfc977d37bb44d2d7da9b51
heatherhaks <heatherannekiefer@gmail.com>
2018-05-31 23:52:39 -0600

    added support for flipping

--- src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
+++ src/com/halfdeadgames/kterminal/KTerminalGlyph.kt
@@ -5,19 +5,21 @@ import com.badlogic.gdx.graphics.Color
 data class KTerminalGlyph(var char: Char,
                           var foreground: Color,
                           var background: Color,
-                          var rotation: Float = 0f) {
-    fun set(char: Char, foregroundColor: Color, backgroundColor: Color, rotation: Float = 0f) {
+                          var rotation: Float = 0f,
+                          var isFlipped: Boolean = false) {
+    fun set(char: Char, foregroundColor: Color, backgroundColor: Color, rotation: Float = 0f, isFlipped: Boolean = false) {
         this.char = char
         this.foreground = foregroundColor.cpy()
         this.background = backgroundColor.cpy()
         this.rotation = rotation
+        this.isFlipped = isFlipped
     }
 
     fun set(glyph: KTerminalGlyph) {
-        set(glyph.char, glyph.foreground, glyph.background, glyph.rotation)
+        set(glyph.char, glyph.foreground, glyph.background, glyph.rotation, glyph.isFlipped)
     }
 
     fun copy() : KTerminalGlyph {
-        return KTerminalGlyph(char,  foreground.cpy(), background.cpy(), rotation)
+        return KTerminalGlyph(char,  foreground.cpy(), background.cpy(), rotation, isFlipped)
     }
 }

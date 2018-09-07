52aa7ef8ab9c505ced8ba77d126bdce86409ae87
Heather Kiefer <heatherannekiefer@gmail.com>
2018-09-07 02:17:29 -0600

    implemented support for writing Int instead of Char

--- src/com/halfdeadgames/kterminal/KTerminalData.kt
+++ src/com/halfdeadgames/kterminal/KTerminalData.kt
@@ -140,10 +140,10 @@ class KTerminalData(width: Int,
         workingCursor.isFlippedX = glyph.isFlippedX
         workingCursor.isFlippedY = glyph.isFlippedY
 
-        write(glyph.char, workingCursor)
+        write(glyph.value, workingCursor)
     }
 
-    @JvmOverloads fun write(char: Char, cursor: Cursor? = null) {
+    @JvmOverloads fun write(value: Int, cursor:Cursor? = null) {
         if(cursor == null) {
             workingCursor.set(this.cursor)
         } else {
@@ -151,7 +151,7 @@ class KTerminalData(width: Int,
         }
 
         terminal[workingCursor.x][workingCursor.y].apply {
-            this.char = char
+            this.value = value
             this.foreground = workingCursor.foregroundColor
             this.background = workingCursor.backgroundColor
             this.rotation = workingCursor.rotation
@@ -159,6 +159,10 @@ class KTerminalData(width: Int,
             this.isFlippedX = workingCursor.isFlippedX
             this.isFlippedY = workingCursor.isFlippedY
         }
+    }
+
+    @JvmOverloads fun write(char: Char, cursor: Cursor? = null) {
+        write(char.toInt(), cursor)
     }
 
     @JvmOverloads fun write(string: String, cursor: Cursor? = null) {
@@ -187,7 +191,7 @@ class KTerminalData(width: Int,
         workingCursor.rotation = 0f
         workingCursor.scale = 1f
 
-        drawRect(width, height, ' ', true, workingCursor)
+        drawRect(width, height, 0, true, workingCursor)
     }
 
     fun clearAll() {
@@ -196,8 +200,16 @@ class KTerminalData(width: Int,
     }
 
     @JvmOverloads fun drawRect(width: Int,
+                               height: Int,
+                               char: Char = ' ',
+                               isFilled: Boolean = true,
+                               cursor: Cursor? = null) {
+        drawRect(width, height, char.toInt(), isFilled, cursor)
+    }
+
+    @JvmOverloads fun drawRect(width: Int,
                  height: Int,
-                 char: Char = ' ',
+                 value: Int,
                  isFilled: Boolean = true,
                  cursor: Cursor? = null) {
         if(cursor == null) {
@@ -214,12 +226,12 @@ class KTerminalData(width: Int,
                 workingCursor.x = i
                 workingCursor.y = j
                 if(isFilled) {
-                    write(char, workingCursor)
+                    write(value, workingCursor)
                 } else {
                     if(i == startX || i == startX + width - 1) {
-                        write(char, workingCursor)
+                        write(value, workingCursor)
                     } else if(j == startY || j == startY + height - 1) {
-                        write(char, workingCursor)
+                        write(value, workingCursor)
                     }
                 }
             }
@@ -227,6 +239,9 @@ class KTerminalData(width: Int,
     }
 
     @JvmOverloads fun drawLine(endX: Int, endY: Int, char: Char, cursor: Cursor? = null) {
+        drawLine(endX, endY, char.toInt(), cursor)
+    }
+    @JvmOverloads fun drawLine(endX: Int, endY: Int, value: Int, cursor: Cursor? = null) {
         if(cursor == null) {
             workingCursor.set(this.cursor)
         } else {
@@ -302,11 +317,11 @@ class KTerminalData(width: Int,
         linePlot.forEach{
             workingCursor.x = it.first
             workingCursor.y = it.second
-            write(char, workingCursor)
+            write(value, workingCursor)
         }
     }
 
-    @JvmOverloads fun drawBox(width: Int, height: Int, topLeft: Char, topRight: Char, bottomLeft: Char, bottomRight: Char, horizontal: Char, vertical: Char, cursor: Cursor? = null) {
+    @JvmOverloads fun drawBox(width: Int, height: Int, topLeft: Int, topRight: Int, bottomLeft: Int, bottomRight: Int, horizontal: Int, vertical: Int, cursor: Cursor? = null) {
         if(cursor == null) {
             workingCursor.set(this.cursor)
         } else {
@@ -331,6 +346,10 @@ class KTerminalData(width: Int,
         write(bottomLeft, workingCursor)
         workingCursor.set(startX + width - 1, startY + height - 1)
         write(bottomRight, workingCursor)
+    }
+
+    @JvmOverloads fun drawBox(width: Int, height: Int, topLeft: Char, topRight: Char, bottomLeft: Char, bottomRight: Char, horizontal: Char, vertical: Char, cursor: Cursor? = null) {
+        drawBox(width, height, topLeft.toInt(), topRight.toInt(), bottomLeft.toInt(), bottomRight.toInt(), horizontal.toInt(), vertical.toInt(), cursor)
     }
 
     @JvmOverloads fun drawDoubleBox(width: Int, height: Int, cursor: Cursor? = null) {
@@ -372,167 +391,167 @@ class KTerminalData(width: Int,
     }
 
     companion object {
-        const val NULL = 0.toChar()
-        const val SMILE = 1.toChar()
-        const val INVERTED_SMILE = 2.toChar()
-        const val HEART = 3.toChar()
-        const val DIAMOND = 4.toChar()
-        const val CLUB = 5.toChar()
-        const val SPADE = 6.toChar()
-        const val BULLET = 7.toChar()
-        const val INVERTED_BULLET = 8.toChar()
-        const val CIRCLE = 9.toChar()
-        const val INVERTED_CIRCLE = 10.toChar()
-        const val MALE = 11.toChar()
-        const val FEMALE = 12.toChar()
-        const val PAIR_EIGHTH_NOTE = 13.toChar()
-        const val EIGHTH_NOTE = 14.toChar()
-        const val SOLAR = 15.toChar()
-        const val RIGHT_TRIANGLE = 16.toChar()
-        const val LEFT_TRIANGLE = 17.toChar()
-        const val UP_DOWN_ARROW = 18.toChar()
-        const val DOUBLE_EXCLAMATION = 19.toChar()
-        const val PARAGRAPH = 20.toChar()
-        const val SECTION = 21.toChar()
-        const val LOWER_BLACK_RECTANGLE = 22.toChar()
-        const val UP_DOWN_BAR_ARROW = 23.toChar()
-        const val UP_ARROW = 24.toChar()
-        const val DOWN_ARROW = 25.toChar()
-        const val RIGHT_ARROW = 26.toChar()
-        const val LEFT_ARROW = 27.toChar()
-        const val RIGHT_ANGLE = 28.toChar()
-        const val LEFT_RIGHT_ARROW = 29.toChar()
-        const val UP_TRIANGLE = 30.toChar()
-        const val DOWN_TRIANGLE = 31.toChar()
-        const val SPACE = 32.toChar()
-        const val HOUSE = 127.toChar()
-        const val UPPER_CEDILLA_C = 128.toChar()
-        const val LOWER_UMLAUT_U = 129.toChar()
-        const val ACUTE_E = 130.toChar()
-        const val CIRCUMFLEX_A = 131.toChar()
-        const val LOWER_UMLAUT_A = 132.toChar()
-        const val GRAVE_A = 133.toChar()
-        const val LOWER_RING_A = 134.toChar()
-        const val LOWER_CEDILLA_C = 135.toChar()
-        const val CIRCUMFLEX_E = 136.toChar()
-        const val UMLAUT_E = 137.toChar()
-        const val GRAVE_E = 138.toChar()
-        const val UMLAUT_I = 139.toChar()
-        const val CIRCUMFLEX_I = 140.toChar()
-        const val GRAVE_I = 141.toChar()
-        const val UPPER_UMLAUT_A = 142.toChar()
-        const val UPPER_RING_A = 143.toChar()
-        const val UPPER_ACUTE_E = 144.toChar()
-        const val LOWER_ASH = 145.toChar()
-        const val UPPER_ASH = 146.toChar()
-        const val CIRCUMFLEX_O = 147.toChar()
-        const val LOWER_UMLAUT_O = 148.toChar()
-        const val GRAVE_O = 149.toChar()
-        const val CIRCUMFLEX_U = 150.toChar()
-        const val GRAVE_U = 151.toChar()
-        const val UMLAUT_Y = 152.toChar()
-        const val UPPER_UMLAUT_O = 153.toChar()
-        const val UPPER_UMLAUT_U = 154.toChar()
-        const val CENT = 155.toChar()
-        const val POUND = 156.toChar()
-        const val YEN = 157.toChar()
-        const val PESETA = 158.toChar()
-        const val HOOK_F = 159.toChar()
-        const val ACUTE_A = 160.toChar()
-        const val ACUTE_I = 161.toChar()
-        const val ACUTE_O = 162.toChar()
-        const val ACUTE_U = 163.toChar()
-        const val LOWER_TILDE_N = 164.toChar()
-        const val UPPER_TILDE_N = 165.toChar()
-        const val ORDINAL_FEMININE = 166.toChar()
-        const val ORDINAL_MASCULINE = 167.toChar()
-        const val INVERTED_QUESTION = 168.toChar()
-        const val INVERTED_NOT = 169.toChar()
-        const val NOT = 170.toChar()
-        const val ONE_HALF = 171.toChar()
-        const val ONE_FOURTH = 172.toChar()
-        const val INVERTED_EXCLAMATION = 173.toChar()
-        const val ANGLE_LEFT = 174.toChar()
-        const val ANGLE_RIGHT = 175.toChar()
-        const val SHADE_LIGHT = 176.toChar()
-        const val SHADE_MEDIUM = 177.toChar()
-        const val SHADE_DARK = 178.toChar()
-        const val BOX_SINGLE_VERTICAL = 179.toChar()
-        const val BOX_SINGLE_VERTICAL_LEFT = 180.toChar()
-        const val BOX_VERTICAL_SINGLE_LEFT_DOUBLE = 181.toChar()
-        const val BOX_VERTICAL_DOUBLE_LEFT_SINGLE = 182.toChar()
-        const val BOX_DOWN_DOUBLE_LEFT_SINGLE = 183.toChar()
-        const val BOX_DOWN_SINGLE_LEFT_DOUBLE = 184.toChar()
-        const val BOX_DOUBLE_VERTICAL_LEFT = 185.toChar()
-        const val BOX_DOUBLE_VERTICAL = 186.toChar()
-        const val BOX_DOUBLE_DOWN_LEFT = 187.toChar()
-        const val BOX_DOUBLE_UP_LEFT = 188.toChar()
-        const val BOX_UP_DOUBLE_LEFT_SINGLE = 189.toChar()
-        const val BOX_UP_SINGLE_LEFT_DOUBLE = 190.toChar()
-        const val BOX_SINGLE_DOWN_LEFT = 191.toChar()
-        const val BOX_SINGLE_UP_RIGHT = 192.toChar()
-        const val BOX_SINGLE_HORIZONTAL_UP = 193.toChar()
-        const val BOX_SINGLE_HORIZONTAL_DOWN = 194.toChar()
-        const val BOX_SINGLE_VERTICAL_RIGHT = 195.toChar()
-        const val BOX_SINGLE_HORIZONTAL = 196.toChar()
-        const val BOX_SINGLE_VERTICAL_HORIZONTAL = 197.toChar()
-        const val BOX_VERTICAL_SINGLE_RIGHT_DOUBLE = 198.toChar()
-        const val BOX_VERTICAL_DOUBLE_RIGHT_SINGLE = 199.toChar()
-        const val BOX_DOUBLE_UP_RIGHT = 200.toChar()
-        const val BOX_DOUBLE_DOWN_RIGHT = 201.toChar()
-        const val BOX_DOUBLE_HORIZONTAL_UP = 202.toChar()
-        const val HOX_DOUBLE_HORIZONTAL_DOWN = 203.toChar()
-        const val BOX_DOUBLE_VERTICAL_RIGHT = 204.toChar()
-        const val BOX_DOUBLE_HORIZONTAL = 205.toChar()
-        const val BOX_DOUBLE_VERTICAL_HORIZONTAL = 206.toChar()
-        const val BOX_UP_SINGLE_HORIZONTAL_DOUBLE = 207.toChar()
-        const val BOX_UP_DOUBLE_HORIZONTAL_SINGLE = 208.toChar()
-        const val BOX_DOWN_SINGLE_HORIZONTAL_DOUBLE = 209.toChar()
-        const val BOX_DOWN_DOUBLE_HORIZONTAL_SINGLE = 210.toChar()
-        const val BOX_UP_DOUBLE_RIGHT_SINGLE = 211.toChar()
-        const val BOX_UP_SINGLE_RIGHT_DOUBLE = 212.toChar()
-        const val BOX_DOWN_SINGLE_RIGHT_DOUBLE = 213.toChar()
-        const val BOX_DOWN_DOUBLE_RIGHT_SINGLE = 214.toChar()
-        const val BOX_VERTICAL_DOUBLE_HORIZONTAL_SINGLE = 215.toChar()
-        const val BOX_VERTICAL_SINGLE_HORIZONTAL_DOUBLE = 216.toChar()
-        const val BOX_SINGLE_UP_LEFT = 217.toChar()
-        const val BOX_SINGLE_DOWN_RIGHT = 218.toChar()
-        const val BLOCK_FULL = 219.toChar()
-        const val BLOCK_LOWER_HALF = 220.toChar()
-        const val BLOCK_LEFT_HALF = 221.toChar()
-        const val BLOCK_RIGHT_HALF = 222.toChar()
-        const val BLOCK_UPPER_HALF = 223.toChar()
-        const val ALPHA = 224.toChar()
-        const val SHARP_S = 225.toChar()
-        const val GAMMA = 226.toChar()
-        const val PI = 227.toChar()
-        const val UPPER_SIGMA = 228.toChar()
-        const val LOWER_SIGMA = 229.toChar()
-        const val MU = 230.toChar()
-        const val TAU = 231.toChar()
-        const val UPPER_PHI = 232.toChar()
-        const val THETA = 233.toChar()
-        const val OMEGA = 234.toChar()
-        const val DELTA = 235.toChar()
-        const val INFINITY = 236.toChar()
-        const val LOWER_PHI = 237.toChar()
-        const val EPSILON = 238.toChar()
-        const val INTERSECTION = 239.toChar()
-        const val TRIPLE_BAR = 240.toChar()
-        const val PLUS_MINUS = 241.toChar()
-        const val GREATER_EQUAL = 242.toChar()
-        const val LESSER_EQUAL = 243.toChar()
-        const val INTEGRAL_TOP = 244.toChar()
-        const val INTEGRAL_BOTTOM = 245.toChar()
-        const val DIVISION = 246.toChar()
-        const val APROXIMATE = 247.toChar()
-        const val DEGREE = 248.toChar()
-        const val SMALL_BULLET = 249.toChar()
-        const val INTERPUNCT = 250.toChar()
-        const val RADICAL = 251.toChar()
-        const val SUPERSCRIPT = 252.toChar()
-        const val SUPERSCRIPT_SQUARE = 253.toChar()
-        const val SQUARE = 254.toChar()
-        const val NBSP = 255.toChar()
+        const val NULL = 0
+        const val SMILE = 1
+        const val INVERTED_SMILE = 2
+        const val HEART = 3
+        const val DIAMOND = 4
+        const val CLUB = 5
+        const val SPADE = 6
+        const val BULLET = 7
+        const val INVERTED_BULLET = 8
+        const val CIRCLE = 9
+        const val INVERTED_CIRCLE = 10
+        const val MALE = 11
+        const val FEMALE = 12
+        const val PAIR_EIGHTH_NOTE = 13
+        const val EIGHTH_NOTE = 14
+        const val SOLAR = 15
+        const val RIGHT_TRIANGLE = 16
+        const val LEFT_TRIANGLE = 17
+        const val UP_DOWN_ARROW = 18
+        const val DOUBLE_EXCLAMATION = 19
+        const val PARAGRAPH = 20
+        const val SECTION = 21
+        const val LOWER_BLACK_RECTANGLE = 22
+        const val UP_DOWN_BAR_ARROW = 23
+        const val UP_ARROW = 24
+        const val DOWN_ARROW = 25
+        const val RIGHT_ARROW = 26
+        const val LEFT_ARROW = 27
+        const val RIGHT_ANGLE = 28
+        const val LEFT_RIGHT_ARROW = 29
+        const val UP_TRIANGLE = 30
+        const val DOWN_TRIANGLE = 31
+        const val SPACE = 32
+        const val HOUSE = 127
+        const val UPPER_CEDILLA_C = 128
+        const val LOWER_UMLAUT_U = 129
+        const val ACUTE_E = 130
+        const val CIRCUMFLEX_A = 131
+        const val LOWER_UMLAUT_A = 132
+        const val GRAVE_A = 133
+        const val LOWER_RING_A = 134
+        const val LOWER_CEDILLA_C = 135
+        const val CIRCUMFLEX_E = 136
+        const val UMLAUT_E = 137
+        const val GRAVE_E = 138
+        const val UMLAUT_I = 139
+        const val CIRCUMFLEX_I = 140
+        const val GRAVE_I = 141
+        const val UPPER_UMLAUT_A = 142
+        const val UPPER_RING_A = 143
+        const val UPPER_ACUTE_E = 144
+        const val LOWER_ASH = 145
+        const val UPPER_ASH = 146
+        const val CIRCUMFLEX_O = 147
+        const val LOWER_UMLAUT_O = 148
+        const val GRAVE_O = 149
+        const val CIRCUMFLEX_U = 150
+        const val GRAVE_U = 151
+        const val UMLAUT_Y = 152
+        const val UPPER_UMLAUT_O = 153
+        const val UPPER_UMLAUT_U = 154
+        const val CENT = 155
+        const val POUND = 156
+        const val YEN = 157
+        const val PESETA = 158
+        const val HOOK_F = 159
+        const val ACUTE_A = 160
+        const val ACUTE_I = 161
+        const val ACUTE_O = 162
+        const val ACUTE_U = 163
+        const val LOWER_TILDE_N = 164
+        const val UPPER_TILDE_N = 165
+        const val ORDINAL_FEMININE = 166
+        const val ORDINAL_MASCULINE = 167
+        const val INVERTED_QUESTION = 168
+        const val INVERTED_NOT = 169
+        const val NOT = 170
+        const val ONE_HALF = 171
+        const val ONE_FOURTH = 172
+        const val INVERTED_EXCLAMATION = 173
+        const val ANGLE_LEFT = 174
+        const val ANGLE_RIGHT = 175
+        const val SHADE_LIGHT = 176
+        const val SHADE_MEDIUM = 177
+        const val SHADE_DARK = 178
+        const val BOX_SINGLE_VERTICAL = 179
+        const val BOX_SINGLE_VERTICAL_LEFT = 180
+        const val BOX_VERTICAL_SINGLE_LEFT_DOUBLE = 181
+        const val BOX_VERTICAL_DOUBLE_LEFT_SINGLE = 182
+        const val BOX_DOWN_DOUBLE_LEFT_SINGLE = 183
+        const val BOX_DOWN_SINGLE_LEFT_DOUBLE = 184
+        const val BOX_DOUBLE_VERTICAL_LEFT = 185
+        const val BOX_DOUBLE_VERTICAL = 186
+        const val BOX_DOUBLE_DOWN_LEFT = 187
+        const val BOX_DOUBLE_UP_LEFT = 188
+        const val BOX_UP_DOUBLE_LEFT_SINGLE = 189
+        const val BOX_UP_SINGLE_LEFT_DOUBLE = 190
+        const val BOX_SINGLE_DOWN_LEFT = 191
+        const val BOX_SINGLE_UP_RIGHT = 192
+        const val BOX_SINGLE_HORIZONTAL_UP = 193
+        const val BOX_SINGLE_HORIZONTAL_DOWN = 194
+        const val BOX_SINGLE_VERTICAL_RIGHT = 195
+        const val BOX_SINGLE_HORIZONTAL = 196
+        const val BOX_SINGLE_VERTICAL_HORIZONTAL = 197
+        const val BOX_VERTICAL_SINGLE_RIGHT_DOUBLE = 198
+        const val BOX_VERTICAL_DOUBLE_RIGHT_SINGLE = 199
+        const val BOX_DOUBLE_UP_RIGHT = 200
+        const val BOX_DOUBLE_DOWN_RIGHT = 201
+        const val BOX_DOUBLE_HORIZONTAL_UP = 202
+        const val HOX_DOUBLE_HORIZONTAL_DOWN = 203
+        const val BOX_DOUBLE_VERTICAL_RIGHT = 204
+        const val BOX_DOUBLE_HORIZONTAL = 205
+        const val BOX_DOUBLE_VERTICAL_HORIZONTAL = 206
+        const val BOX_UP_SINGLE_HORIZONTAL_DOUBLE = 207
+        const val BOX_UP_DOUBLE_HORIZONTAL_SINGLE = 208
+        const val BOX_DOWN_SINGLE_HORIZONTAL_DOUBLE = 209
+        const val BOX_DOWN_DOUBLE_HORIZONTAL_SINGLE = 210
+        const val BOX_UP_DOUBLE_RIGHT_SINGLE = 211
+        const val BOX_UP_SINGLE_RIGHT_DOUBLE = 212
+        const val BOX_DOWN_SINGLE_RIGHT_DOUBLE = 213
+        const val BOX_DOWN_DOUBLE_RIGHT_SINGLE = 214
+        const val BOX_VERTICAL_DOUBLE_HORIZONTAL_SINGLE = 215
+        const val BOX_VERTICAL_SINGLE_HORIZONTAL_DOUBLE = 216
+        const val BOX_SINGLE_UP_LEFT = 217
+        const val BOX_SINGLE_DOWN_RIGHT = 218
+        const val BLOCK_FULL = 219
+        const val BLOCK_LOWER_HALF = 220
+        const val BLOCK_LEFT_HALF = 221
+        const val BLOCK_RIGHT_HALF = 222
+        const val BLOCK_UPPER_HALF = 223
+        const val ALPHA = 224
+        const val SHARP_S = 225
+        const val GAMMA = 226
+        const val PI = 227
+        const val UPPER_SIGMA = 228
+        const val LOWER_SIGMA = 229
+        const val MU = 230
+        const val TAU = 231
+        const val UPPER_PHI = 232
+        const val THETA = 233
+        const val OMEGA = 234
+        const val DELTA = 235
+        const val INFINITY = 236
+        const val LOWER_PHI = 237
+        const val EPSILON = 238
+        const val INTERSECTION = 239
+        const val TRIPLE_BAR = 240
+        const val PLUS_MINUS = 241
+        const val GREATER_EQUAL = 242
+        const val LESSER_EQUAL = 243
+        const val INTEGRAL_TOP = 244
+        const val INTEGRAL_BOTTOM = 245
+        const val DIVISION = 246
+        const val APROXIMATE = 247
+        const val DEGREE = 248
+        const val SMALL_BULLET = 249
+        const val INTERPUNCT = 250
+        const val RADICAL = 251
+        const val SUPERSCRIPT = 252
+        const val SUPERSCRIPT_SQUARE = 253
+        const val SQUARE = 254
+        const val NBSP = 255
     }
 }
